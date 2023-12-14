3e024e301df660084c134c5717e790c878f17904
donnybrilliant <daniel.vier@gmail.com>
2023-12-14 17:53:18 +0100

    Testing new cache dir for deployment

--- server/src/controller.js
+++ server/src/controller.js
@@ -18,8 +18,12 @@ async function takeScreenshot(req, res) {
 
   try {
     const browser = await puppeteer.launch({
-      headless: "new",
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      headless: true,
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disk-cache-dir=./.cache/puppeteer",
+      ],
     });
     const page = await browser.newPage();
     const modifiedUrl =
