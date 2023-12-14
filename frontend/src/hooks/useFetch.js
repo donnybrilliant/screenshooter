9a09b4bb153e65317f0882a1bbe6f930182d48e6
donnybrilliant <daniel.vier@gmail.com>
2023-12-14 15:17:13 +0100

    Fixes base url

--- frontend/src/hooks/useFetch.js
+++ frontend/src/hooks/useFetch.js
@@ -4,7 +4,7 @@ const useFetch = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [downloadUrl, setDownloadUrl] = useState(null);
-  const BASE_URL = "http://localhost:3000";
+  const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
 
   // Check if a string is a valid URL
   const isValidUrl = (url) => {
