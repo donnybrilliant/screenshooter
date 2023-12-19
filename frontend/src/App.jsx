9df62a1dd4652532e82e0fc5dd3edf007e5487c5
donnybrilliant <daniel.vier@gmail.com>
2023-12-19 16:26:18 +0100

    Adds loading state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,26 @@ import Status from "./components/Status";
 const App = () => {
   const { fetchScreenshot, loading, error, downloadUrl } = useFetch();
 
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Example: Wait for window to load all assets
+    window.addEventListener("load", () => {
+      setIsLoading(false);
+    });
+
+    // Optional: Clean up the listener
+    return () => {
+      window.removeEventListener("load", () => {
+        setIsLoading(false);
+      });
+    };
+  }, []);
+
+  if (isLoading) {
+    return <main>Loading...</main>;
+  }
+
   return (
     <main>
       <Input onSubmit={fetchScreenshot} />
